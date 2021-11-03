5e02c788d54e7bbe2f083a143b6274793c993e76
Fares Elmetwally <faresash@hotmail.com>
2021-11-03 03:09:06 -0400

    This is just the basic name entry algorithm.

--- main.cpp
+++ main.cpp
@@ -8,9 +8,10 @@
 using namespace std;
 
 int main() {
+    //----------------------------------------------------------------------------------------------//
+    //------------------------------------Basic Starting Menu---------------------------------------//
 	string input;
 	int playerCount = 0;
-	
 	cout << "                  MONOPOlY\n" 
 	     << "2-8 players.\n" 
 		 << "Once all names are entered, type 'start' to begin.\n" 
@@ -20,7 +21,7 @@ int main() {
 		while (input != "start") {
 			cin >> input;
 			if (playerCount > 8) cout << "Too many players! Please begin game.\n";
-			else if (input != "start"){
+			else if (input != "start") {
 				playerCount++; 
 				//insert new node into player list
 			}
@@ -31,7 +32,18 @@ int main() {
 	}
 	
 	cout << "Enjoy!\n";
-	//call function to begin game in while-loop.cpp
+    //--------------------------------------------------------------------------------------------//
+    //--------------------------------------------------------------------------------------------//
+
+    //--------------------------------------------------------------------------------------------//
+    //----------------------------------Game Function Call----------------------------------------//
+
+
+	                  //call function to begin game in while-loop.cpp
+
+
+    //--------------------------------------------------------------------------------------------//
+    //--------------------------------------------------------------------------------------------//
 		
 	return 0;
 }
