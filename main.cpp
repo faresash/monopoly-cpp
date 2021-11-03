42fffcf861a351302def2b6cc4a699aa0f898201
Fares Elmetwally <faresash@hotmail.com>
2021-11-03 02:37:46 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,38 @@
+#include <iostream>
+#include <string>
+#include <list>
+#include "macros.h"
+#include "functions.h"
+#include "globals.h"
 
+using namespace std;
+using namespace std::string;
+
+int main() {
+	string input;
+	int playerCount = 0;
+	
+	cout << "                  MONOPOlY\n" 
+	     << "2-8 players.\n" 
+		 << "Once all names are entered, type 'start' to begin.\n" 
+		 << "Enter names below:\n";
+		 
+	while 1 {
+		while (input != "start") {
+			cin >> input;
+			if (playerCount > 8) cout << "Too many players! Please begin game.\n"
+			else {
+				playerCount++; 
+				//insert new node into player list
+			}
+		}
+		input.clear();
+		if (playerCount < 2) cout "Too few players! Add more players before starting:\n";
+		else break;
+	}
+	
+	cout << "Enjoy!\n"
+	//call function to begin game in while-loop.cpp
+		
+	return 0;
+}
