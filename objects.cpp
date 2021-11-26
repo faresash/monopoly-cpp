03edec1167e32c8be966e838ce6b1ba0e4189609
Fares Elmetwally <faresash@hotmail.com>
2021-11-26 18:58:49 -0500

    hello

--- objects.cpp
+++ objects.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+#include <string>
+#include <list>
+#include <algorithm>
+#include <array>
+#include <random>
+#include "macros.h"
+#include "functions.h"
+#include "globals.h"
+
+class Square {
+protected:
+    int index;
+    std::string name;
+};
+
+class propertySquare : public Square {
+
+};
+
+class chanceSquare : public Square {
+
+};
+
+class goSquare : public Square {
+
+};
+
+class jailSquare : public Square {
+
+};
+
+class goToJailSquare : public Square {
+
+};
 
