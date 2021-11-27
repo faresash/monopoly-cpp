b0b665e2494a7bc6d04f620e69a7f312126159e5
Fares Elmetwally <faresash@hotmail.com>
2021-11-26 19:55:54 -0500

    u

---
+++ cmake-build-debug/CMakeFiles/monopoly_cpp.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/fares/Documents/GitHub/monopoly-cpp/main.cpp" "C:/Users/fares/Documents/GitHub/monopoly-cpp/cmake-build-debug/CMakeFiles/monopoly_cpp.dir/main.cpp.obj"
+  "C:/Users/fares/Documents/GitHub/monopoly-cpp/objects.cpp" "C:/Users/fares/Documents/GitHub/monopoly-cpp/cmake-build-debug/CMakeFiles/monopoly_cpp.dir/objects.cpp.obj"
+  "C:/Users/fares/Documents/GitHub/monopoly-cpp/user-interface.cpp" "C:/Users/fares/Documents/GitHub/monopoly-cpp/cmake-build-debug/CMakeFiles/monopoly_cpp.dir/user-interface.cpp.obj"
+  "C:/Users/fares/Documents/GitHub/monopoly-cpp/while-loop.cpp" "C:/Users/fares/Documents/GitHub/monopoly-cpp/cmake-build-debug/CMakeFiles/monopoly_cpp.dir/while-loop.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
