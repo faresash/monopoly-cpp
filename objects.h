7caf76f3b4089fa72fc5c338eeb2abb77ec04fa2
Fares Elmetwally <faresash@hotmail.com>
2021-11-26 20:34:36 -0500

    h

--- objects.h
+++ objects.h
@@ -6,6 +6,7 @@ private:
     int id;
     int color;
     int value;
+    std::string name;
 
 public:
     Property() {
@@ -13,15 +14,17 @@ public:
         color = 0;
         value = 0;
     }
-    Property(int ID, int col, int val) {
+    Property(int ID, int col, int val, std::string nam) {
         id = ID;
         color = col;
         value = val;
+        name = nam;
     }
     Property operator = (const Property & other) {
         id = other.id;
         color = other.color;
         value = other.value;
+        name = other.name;
         return *this;
     }
 };
@@ -36,8 +39,8 @@ class propertySquare : public Square {
 private:
     Property property; //each Property Square contains a pointer to a property
 public:
-    propertySquare(int myIndex, std::string myName, int ID, int col, int val) {
-        Property newProp(ID, col, val); //allocating memory for the new property
+    propertySquare(int myIndex, std::string myName, int ID, int col, int val, std::string nam) {
+        Property newProp(ID, col, val, nam); //allocating memory for the new property
         index = myIndex; //setting square index to argument
         name = myName; //setting square name to argument
         property = newProp; //setting property to newly allocated one
