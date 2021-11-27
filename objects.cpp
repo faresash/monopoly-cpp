98c74b22e61405f72f0280a2fde23ce352164bd8
Fares Elmetwally <faresash@hotmail.com>
2021-11-26 19:46:58 -0500

    propertySquare

--- objects.cpp
+++ objects.cpp
@@ -14,7 +14,23 @@ private:
     int color;
     int value;
 
-    Property(int ID, int col, int val) {id = ID; color = col; value = val;}
+public:
+    Property() {
+        id = 0;
+        color = 0;
+        value = 0;
+    }
+    Property(int ID, int col, int val) {
+        id = ID;
+        color = col;
+        value = val;
+    }
+    Property operator = (const Property & other) {
+        id = other.id;
+        color = other.color;
+        value = other.value;
+        return *this;
+    }
 };
 
 class Square {
@@ -24,7 +40,15 @@ protected:
 };
 
 class propertySquare : public Square {
-
+private:
+    Property * property; //each Property Square contains a pointer to a property
+public:
+    propertySquare(int myIndex, std::string myName, int ID, int col, int val) {
+        Property * newProp = new Property(ID, col, val); //allocating memory for the new property
+        index = myIndex; //setting square index to argument
+        name = myName; //setting square name to argument
+        property = newProp; //setting property to newly allocated one
+    }
 };
 
 class chanceSquare : public Square {
